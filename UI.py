013884f05a624588283108c652b4f18eb685fec6
максим <ya.maksim2006@yandex.ru>
2024-12-07 17:21:23 +0300

    3

---
+++ UI.py
@@ -0,0 +1,21 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(0, 0, 401, 61))
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "КнОпОчКа"))
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QMainWindow
 from random import randint
+from UI import Ui_Form
 
 
-class WidgetsHideNSeek(QMainWindow):
+class WidgetsHideNSeek(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.drawKrug)
         self.flag = False
 
@@ -22,11 +22,12 @@ class WidgetsHideNSeek(QMainWindow):
             if self.flag:
                 qp = QPainter()
                 qp.begin(self)
-                qp.setPen(QColor(255, 255, 0))
-                qp.setBrush(QColor(255, 255, 0))
                 for _ in range(5):
-                    cx = randint(0, 400)
-                    cy = randint(61, 300)
+                    color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+                    qp.setPen(color)
+                    qp.setBrush(color)
+                    cx = randint(20, 380)
+                    cy = randint(81, 280)
                     r = randint(20, 50)
                     qp.drawEllipse(cx, cy, r, r)
                 qp.end()
